e68cf955b31318ecab333ad000249dc14921f3e5
Greg Roberts <gregorysroberts@gmail.com>
2014-02-12 19:25:58 -0700

    Cleans up code

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 var messages = ["first message here", "greg is my name"];
 
 var onRequest = function (request, response) {
-	console.log(request.method);
-	response.writeHead(200, {
-		'Connection': 'close',
-		'Content-type': 'text/html',
-		'Access-Control-Allow-Origin': '*'
-	});
 
 	if (request.method == 'GET') {
 		response.end(JSON.stringify(messages));
-    console.log("GET")
-	} 
+	}
+
   else if (request.method == 'POST') {
-    console.log("POST")
     var postData = '';
+
     request.on('data', function(chunk) {
-    postData += chunk.toString();
-   });
+      postData += chunk.toString();
+    });
+
     request.on('end', function() {
-      console.log("Got POST data:");
-      console.log(JSON.parse(postData));
       var message = JSON.parse(postData).message;
       messages.push(message);
+
+      response.writeHead(200, {
+      'Connection': 'close',
+      'Content-type': 'text/html',
+      'Access-Control-Allow-Origin': '*'
+      });
+
       response.end(JSON.stringify(messages));
    });
   }
